0562e061d0fae4fe896b8166d60effa1d72ef1e2
Darian Toledo <80854633+darianmorat@users.noreply.github.com>
2026-08-23 21:24:53 -0500

    fix: random

--- theme-showcase/test.js
+++ theme-showcase/test.js
@@ -83,6 +83,7 @@ class DataProcessor extends EventEmitter {
 
       if (timeout !== undefined && typeof timeout !== "number") {
          errors.push("timeout must be a number")
+         console.log("remove this")
       }
 
       if (maxRetries !== undefined && !Number.isInteger(maxRetries)) {
