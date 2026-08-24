d27939506436895023bd40f1a1afc0573891bf6f
Darian Toledo <80854633+darianmorat@users.noreply.github.com>
2026-08-23 21:37:05 -0500

    commit

--- theme-showcase/test.js
+++ theme-showcase/test.js
@@ -83,6 +83,7 @@ class DataProcessor extends EventEmitter {
 
       if (timeout !== undefined && typeof timeout !== "number") {
          errors.push("timeout must be a number")
+         eee
       }
 
       if (maxRetries !== undefined && !Number.isInteger(maxRetries)) {
